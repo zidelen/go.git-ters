b1870fc7f27a974723cd5a72eee1460f70c9d276
Phillip Henry <phillipjh27@gmail.com>
2024-02-20 16:33:03 -0600

    Reformatted Code

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 // Main.java
 public class Main {
-    public static void main(String[] args) {
-        // Your main code here
-    }
+
+  public static void main(String[] args) {
+    // Your main code here
+  }
 }
--- tests/TestBoardPrint.java
+++ tests/TestBoardPrint.java
@@ -1,6 +1,7 @@
 // testBoardPrint.java
 public class TestBoardPrint {
-    public static void main(String[] args) {
-        Chessboard.printBoard();
-    }
+
+  public static void main(String[] args) {
+    Chessboard.printBoard();
+  }
 }
