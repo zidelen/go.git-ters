46b706e06b7d0cbad77d7a8d8fb6e16db894f2e0
Phillip Henry <phillipjh27@gmail.com>
2024-02-20 13:06:20 -0600

    started making changes to printing board to screen

--- src/Chessboard.java
+++ src/Chessboard.java
@@ -1,4 +1,15 @@
 public class Chessboard {
+    public enum Column {
+        a,b,c,d,e,f,g,h;
+        public static char getLetterForColumn(int columnNumber) {
+            if (columnNumber >= 1 && columnNumber <= values().length) {
+                return values()[columnNumber - 1].name().charAt(0);
+            } else {
+                throw new IllegalArgumentException("Invalid Column Number");
+            }
+        }
+    }
+
 
 
 
